d4b96464c4debc6e2e4541e1d80d2a15f6b64ae0
akash1813 <99417020+akash1813@users.noreply.github.com>
2025-10-02 20:55:56 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -113,8 +113,8 @@ connectDB();
 const PORT = process.env.PORT || 8001;
 const HOST = '0.0.0.0';
 
-const server = app.listen(PORT, HOST, () => {
-    console.log(`Server is running on http://${HOST}:${PORT}`);
+const server = app.listen(PORT,  () => {
+    console.log(`Server is running on port ${PORT}`);
     
     // Handle server errors
     server.on('error', (error) => {
